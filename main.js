25225649c5f4486d8535b6423dd87b3328bea027
Nioqui <Nicolas.l-19@outlook.com>
2024-05-31 22:39:45 -0300

    add google maps api and conecting it with ipAPI, error with undefined ip results and some html and css and close to end

--- main.js
+++ main.js
@@ -1,44 +1,64 @@
-document.getElementById('trackbutton').addEventListener('click', function(event) {
-    event.preventDefault();
-    trackIp();
-});
+let coord = {lat: undefined, lng: undefined};
 
-
-
-function trackIp(){
-    let ip = document.getElementById('search').value;
-    const apiURL = `http://ip-api.com/json/${ip}`;
-
-    fetch(apiURL)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        displayInfo(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        displayError(error.message);
+function iniciarMap(coord) {
+    coord = {lat: -34.4097558385695, lng: -64.59585097748153};
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
+        center: coord,
     });
-
 }
 
-function displayInfo(data) {
-    document.getElementById('ip-result').innerText = data.query;
-    document.getElementById('Location').innerText = data.city + " " + data.region + " " + data.zip;
-    document.getElementById('timezone').innerText = data.timezone;
-    document.getElementById('ISP').innerText = data.isp;
+ document.getElementById('trackbutton').addEventListener('click', function(event) {
+     event.preventDefault();
+     trackIp();
+ });
+
+ function trackIp(coord){
+     let ip = document.getElementById('search').value;
+     const apiURL = `http://ip-api.com/json/${ip}`;
+
+     fetch(apiURL)
+     .then(response => {
+         if (!response.ok) {
+             throw new Error(`HTTP error! status: ${response.status}`);
+         }
+         return response.json();
+     })
+     .then(data => {
+         console.log(data);
+         displayInfo(data);
+         if(data.city == undefined && data.region == undefined && data.zip == undefined){
+            alert('Coloca un ip o dominio valido')
+            document.getElementById('ip-result').innerText = "--";
+            document.getElementById('Location').innerText = "--";
+            document.getElementById('timezone').innerText = "--";
+            document.getElementById('ISP').innerText = "--";
+         } else{
+         coord = {lat: data.lat, lng: data.lon};
+         let map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 13,
+             center: coord,
+         });}
+     })
+     .catch(error => {
+         console.error('Error:', error);
+         displayError(error.message);
+     });
+
+ }
+
+ function displayInfo(data) {
+
+     document.getElementById('ip-result').innerText = data.query;
+     document.getElementById('Location').innerText = data.city + " " + data.region + " " + data.zip;
+     document.getElementById('timezone').innerText = data.timezone;
+     document.getElementById('ISP').innerText = data.isp;
+ }
+
+ function displayError(message) {
+     const infoDiv = document.getElementById('info');
+     infoDiv.innerHTML = `<p>Error: ${message}</p>`;
 }
-
-//function displayError(message) {
-//    const infoDiv = document.getElementById('info');
-//    infoDiv.innerHTML = `<p>Error: ${message}</p>`;
-//}
-
 
 
 
